e448372b26788ad7f8c82934c4ab736ad5710e78
Gov <govinda.escobar@gmail.com>
2021-10-18 22:39:58 -0700

    completed module 5

--- src/App.vue
+++ src/App.vue
@@ -3,26 +3,32 @@
     <nav>
       <ul>
         <li class="nav-item">
-          <img class="logo" src="./assets/build-a-bot-logo.png"/>
-          Build-a-Bot
+          <router-link class="nav-link" :to="{name: 'Home'}">
+            <img class="logo" src="./assets/build-a-bot-logo.png"/>
+            Build-a-Bot
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" :to="{name: 'Build'}">
+            Build
+          </router-link>
         </li>
       </ul>
     </nav>
   </header>
-  <main>
-    <RobotBuilder />
-  </main>
+  <div class="container">
+    <aside class="aside">
+      <router-view name="sidebar"/>
+    </aside>
+    <main>
+      <router-view />
+    </main>
+  </div>
 </template>
 
 <script>
-// import HomePage from './home/HomePage.vue';
-import RobotBuilder from './build/RobotBuilder.vue';
-
 export default {
   name: 'App',
-  components: {
-    RobotBuilder,
-  },
 };
 </script>
 
@@ -38,15 +44,14 @@ body {
   font-family: Avenir, Helvetica, Arial, sans-serif;
 }
 main {
-  margin: 0 auto;
   padding: 30px;
   background-color: white;
-  width: 1024px;
+  width: 964px;
   min-height: 300px;
 }
 header {
   background-color: #999;
-  width: 1084px;
+  width: 1184px;
   margin: 0 auto;
 }
 ul {
@@ -62,5 +67,23 @@ ul {
 .logo {
   vertical-align: middle;
   height: 30px;
+}
+.nav-link {
+  text-decoration: none;
+  color: inherit;
+}
+.router-link-active {
+  color: white;
+}
+.container {
+  display: flex;
+  margin: 10px auto 0 auto;
+  justify-content: center;
+}
+.aside {
+  padding: 30px;
+  background-color: #aaa;
+  width: 100px;
+  min-height: 300px;
 }
 </style>
