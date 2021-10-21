221a601a905fd0fa80744dd6656cde89c2d8f8ab
Gov <govinda.escobar@gmail.com>
2021-10-21 15:55:08 -0700

    completed tutorial' :

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,33 @@
 module.exports = {
+  configureWebpack: (config) => {
+    config.module.rules.push({
+      test: /\.coffee$/,
+      use: ['coffee-loader'],
+    });
+
+    const newRule = {
+      test: /\.(png|jpe?g|gif|webp)(\?.*)?$/,
+      use: [
+        {
+          loader: 'C:\\dev\\vue\\vuejs-fundamentals\\build-a-bot\\node_modules\\url-loader\\dist\\cjs.js',
+          options: {
+            limit: 5000,
+            fallback: {
+              loader: 'C:\\dev\\vue\\vuejs-fundamentals\\build-a-bot\\node_modules\\file-loader\\dist\\cjs.js',
+              options: {
+                name: 'img/[name].[hash:8].[ext]',
+              },
+            },
+          },
+        },
+      ],
+    };
+
+    const imagesRuleIndex = config.module.rules
+      .findIndex((x) => x.test.source.includes('png|jpe?g|gif|webp'));
+
+    config.module.rules.splice(imagesRuleIndex, 1, newRule);
+  },
   devServer: {
     proxy: {
       '/api': {
